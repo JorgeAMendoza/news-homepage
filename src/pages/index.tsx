b7377150bf164d5b620c8ee7923c77e36a9b0cc3
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-04-13 18:45:38 -0500

    set struture of home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -12,7 +13,24 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <main>
+        <h1>News homepage</h1>
+
+        <section>
+          <Image
+            src="/some-image"
+            width={343}
+            height={300}
+            alt="image description"
+          />
+          <h2>main article title</h2>
+          <p>main article text</p>
+          <a href="article">read more</a>
+        </section>
+
+        <section>{/* render various new articles here */}</section>
+        <section>{/* render other news articles here */}</section>
+      </main>
     </>
   );
 }
