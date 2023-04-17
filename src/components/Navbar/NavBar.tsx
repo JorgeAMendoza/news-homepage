652b12c2f9fcaa29adc4142ac6a1369fe89eaf2a
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-04-17 05:41:54 -0500

    create navbar style page and apply base style for mobile view

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -1,59 +1,65 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import Style from './navbar.module.scss';
 
 const NavBar = () => {
   return (
-    <div>
-      <Link href="/" aria-label="navigate back to homepage">
-        <Image
-          src="/images/logo.svg"
-          width={64}
-          height={40}
-          alt="Website logo"
-        />
-      </Link>
-      <button
-        aria-label="click to open navigation menu"
-        aria-controls="nav-menu"
-      >
-        <Image
-          src="/images/icon-menu.svg"
-          width={40}
-          height={17}
-          alt="menu icon"
-        />
-      </button>
-      <nav id="nav-menu" aria-hidden="true">
+    <div className={Style.navbar}>
+      <div className={Style.navbarContainer}>
+        <Link href="/" aria-label="navigate back to homepage">
+          <Image
+            src="/images/logo.svg"
+            width={45}
+            height={28}
+            alt="Website logo"
+          />
+        </Link>
+
         <button
-          aria-label="click button to close the nav menu"
+          aria-label="click to open navigation menu"
           aria-controls="nav-menu"
+          className={Style.openMenuButton}
         >
           <Image
-            src="/images/icon-menu-close.svg"
-            width={30}
-            height={30}
-            alt="close menu icon"
+            src="/images/icon-menu.svg"
+            width={40}
+            height={17}
+            alt="menu icon"
           />
         </button>
 
-        <ul>
-          <li>
-            <a href="home">Home</a>
-          </li>
-          <li>
-            <a href="new">New</a>
-          </li>
-          <li>
-            <a href="popular">Popular</a>
-          </li>
-          <li>
-            <a href="trending">Trending</a>
-          </li>
-          <li>
-            <a href="categories">Categories</a>
-          </li>
-        </ul>
-      </nav>
+        <nav id="nav-menu" aria-hidden="true" className={Style.nav}>
+          <button
+            aria-label="click button to close the nav menu"
+            aria-controls="nav-menu"
+          >
+            <Image
+              src="/images/icon-menu-close.svg"
+              width={30}
+              height={30}
+              alt="close menu icon"
+            />
+          </button>
+
+          <ul className={Style.navLinks}>
+            <li>
+              <a href="home">Home</a>
+            </li>
+            <li>
+              <a href="new">New</a>
+            </li>
+            <li>
+              <a href="popular">Popular</a>
+            </li>
+            <li>
+              <a href="trending">Trending</a>
+            </li>
+            <li>
+              <a href="categories">Categories</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 };
